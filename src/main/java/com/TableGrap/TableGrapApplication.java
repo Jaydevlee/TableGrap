96df5c35c2e0b3398c9c41e65bd6936050a6fd4e
devqw010102 <qw010102@gmail.com>
2025-12-10 16:33:03 +0900

    Add .gitignore

---
+++ src/main/java/com/TableGrap/TableGrapApplication.java
@@ -0,0 +1,13 @@
+package com.TableGrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableGrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableGrapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TableGrap/TableGrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TableGrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableGrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
